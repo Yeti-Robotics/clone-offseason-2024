c831028e171c9706acedbca8411dacc90637f183
boomermath <boomermath13@gmail.com>
2024-06-18 18:51:04 -0400

    Add intake subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+public class IntakeConstants {
+    public static final int INTAKE_MOTOR_ID = 1;
+    public static final int INTAKE_BEAMBREAK_CHANNEL = 0;
+
+    public static final double INTAKE_MOTOR_INTAKE_SPEED = 0.3;
+    public static final double INTAKE_MOTOR_EJECT_SPEED = 0.3;
+    public static final double INTAKE_MOTOR_STOP_SPEED = 0;
+}
